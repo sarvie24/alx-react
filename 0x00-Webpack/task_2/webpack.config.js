d047f2407ba72e54d51ff0a6f65065d1476a78b6
sarvie24 <samuelnnah080@gmail.com>
2024-07-22 17:09:01 -0700

    first commit

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    ['svgo', {
+                        plugins: [
+                            { removeViewBox: false },
+                            { cleanupIDs: false },
+                        ],
+                    }],
+                ],
+            },
+        }),
+    ],
+    resolve: {
+        extensions: ['.js'],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+};
+
